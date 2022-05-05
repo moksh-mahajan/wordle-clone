ff6bde4b2fe32cb5dc86fcadb3876c6966130693
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-05-06 00:15:29 +0530

    Detecting keyboard events and tracking current guess

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -44,33 +45,62 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    Dio().get('http://localhost:3000/solutions').then((response) {
-      // random int between 0 & 14
-      final json = response.data as List;
-      final random = Random();
-      final randomSolution = json[random.nextInt(5)];
-      print('Moksh: ${randomSolution['word']}');
-      setState(() {
-        solution = randomSolution['word'];
-      });
-    });
+    //  Dio().get('http://localhost:3000/solutions').then((response) {
+    // random int between 0 & 14
+    //   final json = response.data as List;
+    //   final random = Random();
+    //   final randomSolution = json[random.nextInt(5)];
+    //   print('Moksh: ${randomSolution['word']}');
+    //   setState(() {
+    //     solution = randomSolution['word'];
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        title: Text('Wordle'),
-        foregroundColor: Colors.black,
-      ),
-      body: Column(children: [
-        Divider(
-          height: 40,
+    return RawKeyboardListener(
+      onKey: (event) {
+        // print(event);
+        if (event is RawKeyUpEvent) {
+          print(event.data.keyLabel);
+
+          if (event.logicalKey == LogicalKeyboardKey.backspace &&
+              currentGuess.isNotEmpty) {
+            setState(() {
+              currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+            });
+            return;
+          }
+
+          if (currentGuess.length < 5) {
+            setState(() {
+              currentGuess += event.data.keyLabel;
+            });
+          }
+        }
+
+        // if (event is RawKeyUpEvent) {
+        //   print(event.character);
+        //   print(event.data);
+        // }
+      },
+      autofocus: true,
+      focusNode: FocusNode(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: Text('Wordle'),
+          foregroundColor: Colors.black,
         ),
-        if (solution != null) Text('Solution is: $solution')
-      ]),
+        body: Column(children: [
+          Divider(
+            height: 40,
+          ),
+          Text('Current guess - $currentGuess')
+        ]),
+      ),
     );
   }
 }
