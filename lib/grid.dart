6d8dc78c507fd3be98bc168adf7d5c5cead779d0
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-05-06 12:45:35 +0530

    Animation tiles

--- lib/grid.dart
+++ lib/grid.dart
@@ -1,10 +1,13 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 
-class Grid extends StatelessWidget {
+class Grid extends StatefulWidget {
   final String currentGuess;
   final List guesses;
   final int turn;
-  const Grid({
+
+  Grid({
     Key? key,
     required this.currentGuess,
     required this.guesses,
@@ -12,37 +15,150 @@ class Grid extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<Grid> createState() => _GridState();
+}
+
+class _GridState extends State<Grid> {
+  // final controller = FlipCardController();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Future.delayed(const Duration(seconds: 0), () {
+  //     controller.toggleCard();
+  //   });
+  // }
+
+  @override
   Widget build(BuildContext context) {
+    // return FlipCard(
+    //   controller: controller,
+    //   flipOnTouch: false,
+    //   fill: Fill
+    //       .fillBack, // Fill the back side of the card to make in the same size as the front.
+    //   direction: FlipDirection.VERTICAL, // default
+    //   front: Container(
+    //     height: 60,
+    //     width: 60,
+    //     alignment: Alignment.center,
+    //     child: Text(
+    //       'R',
+    //       style: Theme.of(context).textTheme.displaySmall,
+    //     ),
+    //     margin: EdgeInsets.all(4),
+    //     decoration: BoxDecoration(border: Border.all()),
+    //   ),
+    //   back: Container(
+    //     height: 60,
+    //     width: 60,
+    //     alignment: Alignment.center,
+    //     child: Text(
+    //       'M',
+    //       style: Theme.of(context).textTheme.displaySmall,
+    //     ),
+    //     margin: EdgeInsets.all(4),
+    //     decoration: BoxDecoration(border: Border.all()),
+    //   ),
+    // );
     return Column(
-        children: guesses.asMap().entries.map((entry) {
-      if (turn == entry.key) {
+        children: widget.guesses.asMap().entries.map((entry) {
+      if (widget.turn == entry.key) {
         return GridRow(
-          currentGuess: currentGuess,
+          currentGuess: widget.currentGuess,
         );
       }
       return GridRow(
         guess: entry.value,
+        flip: entry.key == widget.turn - 1,
       );
     }).toList());
   }
 }
 
-class GridRow extends StatelessWidget {
+class GridRow extends StatefulWidget {
   final String? currentGuess;
   final List? guess;
+  final bool flip;
 
   const GridRow({
     Key? key,
+    this.flip = false,
     this.guess,
     this.currentGuess,
   }) : super(key: key);
 
   @override
+  State<GridRow> createState() => _GridRowState();
+}
+
+class _GridRowState extends State<GridRow> {
+  var _isFlipped = false;
+  final _flipControllers = List.generate(5, (index) => FlipCardController());
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> flipCards() async {
+    for (final controller in _flipControllers) {
+      controller.toggleCard();
+      await Future.delayed(Duration(milliseconds: 150));
+    }
+    _isFlipped = true;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (guess != null) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      // executes after build
+      if (widget.flip && !_isFlipped) {
+        flipCards();
+      }
+    });
+    if (widget.guess != null) {
+      if (widget.flip) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widget.guess!
+              .asMap()
+              .entries
+              .map((entry) => FlipCard(
+                    controller: _flipControllers[entry.key],
+                    flipOnTouch: false,
+                    fill: Fill.fillBack,
+                    direction: FlipDirection.VERTICAL,
+                    front: Container(
+                      height: 60,
+                      width: 60,
+                      alignment: Alignment.center,
+                      child: Text(
+                        entry.value['key'].toUpperCase(),
+                        style: Theme.of(context).textTheme.displaySmall,
+                      ),
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(border: Border.all()),
+                    ),
+                    back: Container(
+                      height: 60,
+                      width: 60,
+                      alignment: Alignment.center,
+                      child: Text(
+                        entry.value['key'].toUpperCase(),
+                        style: Theme.of(context).textTheme.displaySmall,
+                      ),
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                          color: entry.value['color'],
+                          border: Border.all(color: entry.value['color'])),
+                    ),
+                  ))
+              .toList(),
+        );
+      }
       return Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: guess!
+        children: widget.guess!
             .map((e) => Container(
                   height: 60,
                   width: 60,
@@ -53,14 +169,16 @@ class GridRow extends StatelessWidget {
                   ),
                   margin: EdgeInsets.all(4),
                   decoration: BoxDecoration(
-                      color: e['color'], border: Border.all(color: e['color'])),
+                      color: e['color'],
+                      border: Border.all(
+                          color: widget.flip ? Colors.red : e['color'])),
                 ))
             .toList(),
       );
     }
 
-    if (currentGuess != null) {
-      final letters = currentGuess!.split('');
+    if (widget.currentGuess != null) {
+      final letters = widget.currentGuess!.split('');
       return Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
