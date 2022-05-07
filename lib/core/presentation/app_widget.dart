46f3c36832234ce49c19c607613beb041ff9a5d1
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-05-07 17:53:42 +0530

    Project restructuring

---
+++ lib/core/presentation/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../guess/presentation/pages/guess_word_page.dart';
+
+class WordleApp extends StatelessWidget {
+  const WordleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: GuessWordPage(),
+    );
+  }
+}
