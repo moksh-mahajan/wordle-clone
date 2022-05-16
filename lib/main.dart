85e4ec57e3b444a43ff597c98b6c8e593e40453b
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-05-17 00:04:35 +0530

    Added freezed and riverpod packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/presentation/app_widget.dart';
 
 void main() {
-  runApp(const WordleApp());
+  runApp(const ProviderScope(child: WordleApp()));
 }
